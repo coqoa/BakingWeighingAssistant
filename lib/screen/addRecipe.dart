2466323b1b0d04566d9aa889b0b0b9c9cd23ca1a
coqoa <choibm0208@gmail.com>
2023-01-27 23:48:45 +0900

    ok
    ok

--- lib/screen/addRecipe.dart
+++ lib/screen/addRecipe.dart
@@ -2,9 +2,6 @@
 
 import 'package:bwa/config/palette.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/basic.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AddRecipe extends StatefulWidget {
@@ -17,15 +14,29 @@ class AddRecipe extends StatefulWidget {
 class _AddRecipeState extends State<AddRecipe> {
 
   late List listTest;
+  late List listTestKey;
+  late List listTestValue;
+  String titleA = '';
   String testA = '';
   String testB = '';
 
+  void createRecipe(){
+    if(titleA.isNotEmpty){
+      print('-========================-');
+      print('titleA = $titleA');
+      print('listTest = $listTest');
+      print('-========================-');
+    }else{
+      print('TITLE을 채워주세용');
+    }
+  }
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     listTest = [];
-
+    listTestKey = [];
+    listTestValue = [];
   }
   @override
   Widget build(BuildContext context) {
@@ -35,49 +46,79 @@ class _AddRecipeState extends State<AddRecipe> {
       child: Align( // 키보드올라오면 전체 기장 짧아져야함
         alignment: Alignment.topCenter,
         child: Container(
-          width: 330.w,
+          // width: 330.w,
           // height: 820.h,
-          height: 520.h,
+          height: 580.h,
           // color: Colors.green,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: (){
-                      Navigator.of(context).pop();
-                    },
-                    child: Container(
-                      width: 50,
-                      height: 50,
-                      color: Colors.blue,
-                      child: Center(child: Text('<')),
+              Container(
+                decoration: BoxDecoration(
+                  color: Palette.white,
+                  boxShadow: [
+                     BoxShadow(
+                      blurRadius: 5,
+                      offset: Offset(0, 0),
+                      color: Color.fromRGBO(0, 0, 0, .15),
+                    )
+                  ]
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: (){
+                        Navigator.of(context).pop();
+                      },
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        color: Palette.white,
+                        child: Center(child: Text('<')),
+                      ),
                     ),
-                  ),
-                  Text('ADD'),
-                  GestureDetector(
-                    onTap: (){
-                      print('-========================-');
-                      print(listTest);
-                      print('-========================-');
-                    },
-                    child: Container(
-                      width: 50,
-                      height: 50,
-                      color: Colors.orange,
-                      child: Center(child: Text('완료')),
+                    Text('ADD'),
+                    GestureDetector(
+                      onTap: (){
+                        createRecipe();
+                      },
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        color: Palette.white,
+                        child: Center(child: Text('완료')),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-      
+              Container(
+                width: 330.w,
+                child: TextField(
+                  textInputAction: TextInputAction.next,
+                  autofocus: true,
+                  style: TextStyle(
+                    fontSize: 25
+                  ),
+                  onChanged: (value) => titleA = value,
+                  textAlign: TextAlign.center,
+                  decoration: const InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(width: 1, color: Colors.black),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 1, color: Colors.transparent),
+                    ),
+                    hintText: "Title"
+                  ),
+                ),
+              ),
               // 행, 열 기준
               Container(
                 width: 330.w,
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   // color: Colors.red,
                   border: Border(bottom: BorderSide(width: 1, color: Palette.lightgray))
                 ),
@@ -105,7 +146,7 @@ class _AddRecipeState extends State<AddRecipe> {
               Container(
                 width: 330.w,
                 // height: 700.h,
-                height: 400.h,
+                height: 310.h,
                 // color: Colors.green,
                 // padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
                 child : ListView.builder(
@@ -118,24 +159,20 @@ class _AddRecipeState extends State<AddRecipe> {
                             Container(
                               width: 165.w,
                               height: 50.h,
-                              decoration: BoxDecoration(
+                              decoration: const BoxDecoration(
                                 border: Border(right: BorderSide(width: 0.5, color: Palette.reallightgray))
                               ),
                               // color: Colors.red,
                               // child: Center(child: Text(listTest[index]['이름'])),
                               child: TextField(
                                 textInputAction: TextInputAction.next,
-                                onChanged: (value) => testA = value,
                                 textAlign: TextAlign.center,
-                                decoration: InputDecoration(
-                                  focusedBorder: OutlineInputBorder(
-                                    // borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                                    borderSide: BorderSide(width: 1, color: Colors.transparent),
-                                  ),
-                                  enabledBorder: OutlineInputBorder(
-                                    // borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                                    borderSide: BorderSide(width: 1, color: Colors.transparent),
-                                  ),
+                                onChanged: (value) {
+                                  testA = value;
+                                },
+                                decoration: const InputDecoration(
+                                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(width: 1, color: Colors.transparent),),
+                                  enabledBorder: OutlineInputBorder(borderSide: BorderSide(width: 1, color: Colors.transparent),),
                                 ),
                                 // onSubmitted: (value) => testA = value,
                               ),
@@ -144,7 +181,7 @@ class _AddRecipeState extends State<AddRecipe> {
                             Container(
                               width: 165.w,
                               height: 50.h,
-                              decoration: BoxDecoration(
+                              decoration: const BoxDecoration(
                                 border: Border(left: BorderSide(width: 0.5, color: Palette.reallightgray))
                               ),
                               // color: Colors.green,
@@ -153,23 +190,19 @@ class _AddRecipeState extends State<AddRecipe> {
                               child: TextField(
                                 textInputAction: TextInputAction.next,
                                 textAlign: TextAlign.center,
-                                decoration: InputDecoration(
-                                  focusedBorder: OutlineInputBorder(
-                                    // borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                                    borderSide: BorderSide(width: 1, color: Colors.transparent),
-                                  ),
-                                  enabledBorder: OutlineInputBorder(
-                                    // borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                                    borderSide: BorderSide(width: 1, color: Colors.transparent),
-                                  ),
+                                onChanged: (value) {
+                                  testB = value;
+                                } ,
+                                onSubmitted: ((value) {
+                                  // setState(() {
+                                  //   listTest.add("{'이름':$testA : '중량':$testB}"); // ADD하는식이 아니라 다른식으로 해야할ㄷ것같은데
+                                  // });
+                                  print(index);
+                                }),
+                                decoration: const InputDecoration(
+                                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(width: 1, color: Colors.transparent),),
+                                  enabledBorder: OutlineInputBorder(borderSide: BorderSide(width: 1, color: Colors.transparent),),
                                 ),
-                                onChanged: (value) => testB = value,
-                                onSubmitted: (value){
-                                  setState(() {
-                                    listTest.add("{'이름':$testA : '중량':$testB}");
-                                  });
-              
-                                },
                               ),
                             ),
                           ]
