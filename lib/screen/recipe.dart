2240a2582d0156404efa2ffd6be71b4bf2c10ec4
coqoa <choibm0208@gmail.com>
2023-03-30 18:54:51 +0900

    고쳐야 될 사항 생각

--- lib/screen/recipe.dart
+++ lib/screen/recipe.dart
@@ -114,7 +114,7 @@ class _RecipeState extends State<Recipe> {
                           ),
                           onTap: (){
                             setState(() {
-                              Get.off(()=>Menu()); // NOTE: to 로 할지 off로 할지 안드로이드로 확인해야함
+                              Get.off(()=>Menu()); // NOTE: to 로 할지 off로 할지 안드로이드로 확인해야함 //! get back?
                             });
                           },
                         ),
