ce3b2470dce9dc560801b612fe47097f4e33695d
coqoa <choibm0208@gmail.com>
2023-04-16 22:38:28 +0900

    validation 코드정리

--- lib/widget/validation.dart
+++ lib/widget/validation.dart
@@ -1,37 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import '../config/palette.dart';
 
 class Validation{
 
   validationSnackBar(errorCode){
     return  Get.snackbar(
-        "","",
-        titleText: const Center(
-          child: Text("Error", 
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 15,
-              color: Colors.white
-            )
+      "","",
+      titleText: const Center(
+        child: Text("Error", 
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+            color: Colors.white
           )
-        ),
-        messageText: Center(
-          child: Text('$errorCode',
-            style: TextStyle(
-              color: Colors.white
-            ),
-          )
-        ),
-        snackPosition: SnackPosition.BOTTOM,
-        duration: Duration(milliseconds: 3000),
-        forwardAnimationCurve: Curves.easeInOutQuart,
-        reverseAnimationCurve: Curves.easeInQuart,
-        backgroundColor: Palette.black.withOpacity(0.5),
-        margin: EdgeInsets.only(bottom: 20),
-        maxWidth: 300,
-      );
-}
-
+        )
+      ),
+      messageText: Center(
+        child: Text('$errorCode',
+          style: TextStyle(
+            color: Colors.white
+          ),
+        )
+      ),
+      snackPosition: SnackPosition.BOTTOM,
+      duration: Duration(milliseconds: 3000),
+      forwardAnimationCurve: Curves.easeInOutQuart,
+      reverseAnimationCurve: Curves.easeInQuart,
+      backgroundColor: Palette.black.withOpacity(0.5),
+      margin: EdgeInsets.only(bottom: 20),
+      maxWidth: 300,
+    );
+  }
 }
